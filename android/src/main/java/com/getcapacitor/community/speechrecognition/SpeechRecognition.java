560345d4f122a942bb0789e12d7b05b1479159d4
Tam <trieutulong89kg@yahoo.com>
2021-10-13 13:33:29 +0700

    Check permission with ContextCompat

--- android/src/main/java/com/getcapacitor/community/speechrecognition/SpeechRecognition.java
+++ android/src/main/java/com/getcapacitor/community/speechrecognition/SpeechRecognition.java
@@ -1,8 +1,11 @@
 package com.getcapacitor.community.speechrecognition;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.util.Log;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.PermissionState;
 import com.getcapacitor.Plugin;
@@ -71,12 +74,18 @@ public class SpeechRecognition extends Plugin implements Constants {
     }
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      bridge
-        .getActivity()
-        .requestPermissions(
-          new String[] { Manifest.permission.RECORD_AUDIO },
-          REQUEST_CODE_PERMISSION
-        );
+      //      bridge
+      //        .getActivity()
+      //        .requestPermissions(
+      //          new String[]{Manifest.permission.RECORD_AUDIO},
+      //          REQUEST_CODE_PERMISSION
+      //        );
+
+      ActivityCompat.requestPermissions(
+        bridge.getActivity(),
+        new String[] { Manifest.permission.RECORD_AUDIO },
+        REQUEST_CODE_PERMISSION
+      );
     }
     call.resolve();
   }
@@ -85,9 +94,21 @@ public class SpeechRecognition extends Plugin implements Constants {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
       return true;
     }
+    //    PermissionState permissionState = getPermissionState(type);
+    //    return permissionState == PermissionState.GRANTED;
 
-    PermissionState permissionState = getPermissionState(type);
-    return permissionState == PermissionState.GRANTED;
+    int test = ContextCompat.checkSelfPermission(
+      bridge.getActivity(),
+      Manifest.permission.RECORD_AUDIO
+    );
+
+    return (
+      ContextCompat.checkSelfPermission(
+        bridge.getActivity(),
+        Manifest.permission.RECORD_AUDIO
+      ) ==
+      PackageManager.PERMISSION_GRANTED
+    );
   }
 
   private void fromMic(
